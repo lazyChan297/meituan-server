b9d8ea68ec8d0edd87ec74906031f70bf31e8f39
lazyChan297 <lazyChan297@163.com>
2018-10-18 18:15:29 +0800

    新增处理跨域请求

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const app = new Koa()
 const bodyParser = require('./middlewares/bodyparser')
 // const config = require('./config')
 
+const cors = require('koa2-cors');
+
+app.use(cors({
+    origin: function (ctx) {
+        if (ctx.url === '/test') {
+            return "*"; // 允许来自所有域名请求
+        }
+        return 'http://localhost:8080'; 
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept']
+}))
+
+
+
 // 使用响应处理中间件
 // app.use(response)
 
